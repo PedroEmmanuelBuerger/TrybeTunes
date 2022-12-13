a61e4863eef7224df52d71a729cee4c4da1aa2b5
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-12-13 18:09:01 -0300

    requesito 2

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Album from '../pages/Album';
 import Favorites from '../pages/Favorites';
 import Login from '../pages/Login';
@@ -7,9 +7,53 @@ import NotFound from '../pages/NotFound';
 import Profile from '../pages/Profile';
 import ProfileEdit from '../pages/ProfileEdit';
 import Search from '../pages/Search';
+import { createUser } from '../services/userAPI';
 
 class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisabled: true,
+      nome: 'Name',
+      Loadings: false,
+      redirect: false,
+    };
+  }
+
+  createUsers = async (par) => {
+    this.setState(({
+      Loadings: true,
+    }));
+    await createUser(par);
+    this.setState(({
+      Loadings: false,
+      redirect: true,
+    }));
+  };
+
+  Disabled = (e) => {
+    const valor = e.target.value;
+    const { nome } = this.state;
+    const name = nome;
+    if (nome === name) {
+      const numero = 3;
+      if (valor.length >= numero) {
+        this.setState(({
+          isDisabled: false,
+        }));
+      } else {
+        this.setState(({
+          isDisabled: true,
+        }));
+      }
+      this.setState(({
+        nome: valor,
+      }));
+    }
+  };
+
   render() {
+    const { isDisabled, nome, Loadings, redirect } = this.state;
     return (
       <main>
         <Switch>
@@ -18,7 +62,18 @@ class Content extends Component {
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="/profile" component={ Profile } />
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+          >
+            { redirect ? <Redirect to="/search" /> : <Login
+              isDisabled={ isDisabled }
+              Disabled={ this.Disabled }
+              nome={ nome }
+              createUsers={ this.createUsers }
+              Loadings={ Loadings }
+            /> }
+          </Route>
           <Route path="*" component={ NotFound } />
         </Switch>
       </main>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
 
 class Login extends Component {
   render() {
+    const { isDisabled, Disabled, nome, createUsers, Loadings } = this.props;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <label htmlFor="name">
+          <input
+            type="text"
+            id="name"
+            data-testid="login-name-input"
+            onChange={ Disabled }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ isDisabled }
+          onClick={ () => createUsers({ name: nome }) }
+        >
+          Entrar
+        </button>
+        {Loadings ? <Loading /> : ''}
+      </div>
     );
   }
 }
 
+Login.propTypes = {
+  isDisabled: PropTypes.bool.isRequired,
+  Disabled: PropTypes.func.isRequired,
+  nome: PropTypes.string.isRequired,
+  createUsers: PropTypes.func.isRequired,
+  Loadings: PropTypes.bool.isRequired,
+};
 export default Login;
