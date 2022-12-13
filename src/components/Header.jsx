e4bcda7f3a76f62d4a7531865f0abc46263c832b
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-12-13 19:04:51 -0300

    requesito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -23,6 +24,11 @@ class Header extends Component {
         <span data-testid="header-user-name">
           { (alredy ? name : <Loading />)}
         </span>
+        <section>
+          <Link to="/search" data-testid="link-to-search"> Pesquisa </Link>
+          <Link to="/favorites" data-testid="link-to-favorites"> Musicas Favoritas </Link>
+          <Link to="/profile" data-testid="link-to-profile"> Perfil </Link>
+        </section>
       </header>
     );
   }
