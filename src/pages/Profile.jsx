9539da7cbcecb00fb77f7b2b9b76ad78d8f2661d
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-01-07 15:57:56 -0300

    requesito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    loadings: false,
+    info: [],
+  };
+
+  componentDidMount() {
+    this.getInfo();
+  }
+
+  getInfo = async () => {
+    this.setState(() => ({
+      loadings: true,
+    }));
+    const informations = await getUser();
+    this.setState(() => ({
+      loadings: false,
+      info: informations,
+    }));
+  };
+
   render() {
+    const { loadings, info } = this.state;
     return (
       <div data-testid="page-profile">
+        {loadings && <Loading />}
         <Header />
+        <section>
+          <div>
+            <h1>Nome</h1>
+            <p>{info.name}</p>
+          </div>
+          <div>
+            <img data-testid="profile-image" src={ info.image } alt="foto" />
+          </div>
+          <div>
+            <h2>Email</h2>
+            <p>{info.email}</p>
+          </div>
+          <div>
+            <h2>Descrição</h2>
+            <p>{info.description}</p>
+          </div>
+          <div>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        </section>
       </div>
     );
   }
